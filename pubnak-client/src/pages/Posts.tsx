eff87a0e47e2e7d833f124c530818a58428fb7f3
AnasNAK <anasc00ding@gmail.com>
2024-04-23 06:44:54 +0100

    update the component posts with some features in front

---
+++ pubnak-client/src/pages/Posts.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from 'next/link';
+import Post from '../components/pages/post/post';
+
+
+
+const post: React.FC = () => {
+  return (
+    <div >
+      <Post />
+     
+    </div>
+  );
+};
+
+export default post;
