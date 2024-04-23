199529aae72fc6934c4cb7102e8f1d92e36f3097
AnasNAK <anasc00ding@gmail.com>
2024-04-23 06:37:29 +0100

    login component with css

---
+++ pubnak-client/src/components/pages/auth/Login.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styles from '../../../styles/index.module.css';
+import Image from 'next/image';
+import left from '../../../../public/images/signup-left.png';
+import imgright from '../../../../public/images/singup-right.png';
+import logo from '../../../../public/images/logo-filerouge-white 1.png';
+import Link from 'next/link';
+
+const LoginPage: React.FC = () => {
+  return (
+    <div className={`${styles.bodyOverlay} flex relative justify-center items-center h-screen `}>
+        <div className=' w-[20rem] absolute z-[-1] top-12 right-[15rem]'>
+            <Image src={imgright} alt='text' />
+        </div>
+        <div className=' w-[20rem] absolute z-[-1] bottom-12 left-[15rem]'>
+            <Image src={left} alt='text' />
+        </div>
+       
+      <div className="flex flex-col md:flex-row">
+        <div className="flex-1 p-8">
+          <div className="bg-white rounded-lg shadow-md p-8 w-full">
+            <div className="flex items-center justify-center mb-6">
+            <Image src={logo} alt='text' className='w-[9rem]' />
+             
+            </div>
+            <h3 className="text-xl font-bold mb-4 text-center">Log In</h3>
+            <p className="text-gray-600 mb-6">Start connecting with influencers and clients</p>
+            <form>
+              <div className="mb-4">
+                <label htmlFor="email" className="block font-bold mb-2">Email</label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="Enter your email address"
+                  className="w-full px-4 py-2 rounded-lg bg-gray-100 border border-gray-300 focus:outline-none focus:ring focus:ring-green-300"
+                />
+              </div>
+              <div className="mb-6">
+                <label htmlFor="password" className="block font-bold mb-2">Password</label>
+                <div className="relative">
+                  <input
+                    id="password"
+                    type="password"
+                    placeholder="Enter a strong password"
+                    className="w-full px-4 py-2 rounded-lg bg-gray-100 border border-gray-300 focus:outline-none focus:ring focus:ring-green-300"
+                  />
+                  <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
+                    <button type="button" className="text-gray-500 hover:text-gray-700">
+                      <i className="fa fa-eye"></i>
+                    </button>
+                  </div>
+                </div>
+                <Link href="/Register" className='text-m text-right float-right text-green-500 cursor-pointer underline my-3'>I Don't Have An Accout</Link>
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600 transition-colors duration-300"
+              >
+                Log in
+              </button>
+            </form>
+          </div>
+        </div>
+       
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
