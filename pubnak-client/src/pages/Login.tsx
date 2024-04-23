b3552512e1426085262a7761f998483ccc3f6cee
AnasNAK <anasc00ding@gmail.com>
2024-04-23 06:41:03 +0100

    update register and login component

---
+++ pubnak-client/src/pages/Login.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import LoginPage from '../components/pages/auth/Login';
+
+const Login: React.FC = () => {
+  return <LoginPage />;
+};
+
+export default Login;
---
+++ pubnak-client/src/pages/Register.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RegisterPage from '../components/pages/auth/Register';
+
+const Register: React.FC = () => {
+  return <RegisterPage />;
+};
+
+export default Register;
