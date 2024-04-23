3880c6243aaa80f564404a78e4907aaf5686c27c
AnasNAK <anasc00ding@gmail.com>
2024-04-23 06:44:06 +0100

    update feed component

---
+++ pubnak-client/src/pages/Feed.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import FeedContent from '../components/pages/Feed/Feed'
+
+ const Feed:React.FC = () => {
+  return (
+    <div>
+        <FeedContent />
+    </div>
+  )
+}
+export default Feed;
