b86fb8e3f87c3ab98006d0ca2ef1ae7866c22175
AnasNAK <anasc00ding@gmail.com>
2024-04-23 06:39:06 +0100

    hero section component with css

--- pubnak-client/src/components/pages/HomePage/HeroSection/HeroSection.tsx
+++ pubnak-client/src/components/pages/HomePage/HeroSection/HeroSection.tsx
@@ -1,16 +1,20 @@
-// HeroSection.tsx
 import React from 'react';
 import Image from 'next/image';
-// import heroImage from '../../assets/hero-image.png';
+import heroImage from '../../../../../public/images/hero-car.png';
 import HeroContent from './HeroContent';
 import HeroStats from './HeroStats';
 import styles from './HeroSection.module.css';
 
 const HeroSection: React.FC = () => {
+  
   return (
-    <section className={styles.heroSection}>
-      <div className={styles.heroImageContainer}>
-        {/* <Image src={heroImage} alt="Hero Image" className={styles.heroImage} /> */}
+    <section className=''>
+
+      <div className='bg-black w-1/3 z-10 h-[35rem] float-end relative '>
+
+        <div className={` w-[300px]  absolute top-[9rem] right-[17rem]  ${styles.heroImageContainer}`} >
+          <Image src={heroImage} alt="Hero Image" className={styles.heroImage} />
+        </div>
       </div>
       <HeroContent />
       <HeroStats />
