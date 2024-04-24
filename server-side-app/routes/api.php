95770ad29718f3b1144f6bbf01815d38bc917cbb
AnasNAK <anasc00ding@gmail.com>
2024-04-24 15:41:00 +0100

    update the api route

---
+++ server-side-app/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    // Route::post('me', [AuthController::class, 'me']);
+});
+
+
+Route::apiResource('category',CategoryController::class);
+
+
