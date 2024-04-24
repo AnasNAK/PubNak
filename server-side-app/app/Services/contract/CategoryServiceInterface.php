118ad11d9e49148043fa3917149ae06ca0d8e28b
AnasNAK <anasc00ding@gmail.com>
2024-04-24 15:38:38 +0100

    create category service interface

---
+++ server-side-app/app/Services/contract/CategoryServiceInterface.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+namespace App\Services\contract;
+
+use App\Models\Category;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
+interface CategoryServiceInterface{
+
+    public function all();
+
+    public function store(StoreCategoryRequest $request);
+
+    public function show(Category $category);
+
+    public function update(Category $category ,UpdateCategoryRequest $request);
+
+    public function delete(Category $category);
+
+
+}
