58e5a80ad0dde703f195a880738acbbb5d74512a
AnasNAK <anasc00ding@gmail.com>
2024-04-24 15:39:02 +0100

    create user service interface

---
+++ server-side-app/app/Services/contract/UserServiceInterface.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace App\Services\contract;
+
+use App\Models\User;
+
+interface UserServiceInterface{
+
+
+    public function all();
+
+    // public function store(User $user);
+
+    // public function show(User $user);
+
+    // public function update(User $user);
+
+    // public function delete(User $user);
+}
