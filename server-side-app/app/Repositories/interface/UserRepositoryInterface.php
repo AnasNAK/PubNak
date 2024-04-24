758a05dbbb2313c37addfdc6866e5ef4db0df3f8
AnasNAK <anasc00ding@gmail.com>
2024-04-24 15:38:23 +0100

    create user repository interface

---
+++ server-side-app/app/Repositories/interface/UserRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace App\Repositories\interface;
+
+use App\Models\User;
+
+interface UserRepositoryInterface{
+
+
+    public function all();
+
+    // public function store(User $user);
+
+    // public function show(User $user);
+
+    // public function update(User $user);
+
+    // public function delete(User $user);
+}
