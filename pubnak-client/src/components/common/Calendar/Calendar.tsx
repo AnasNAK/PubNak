6361a8f25c616080dd17dbfc72ec581c6016c546
AnasNAK <anasc00ding@gmail.com>
2024-04-24 15:33:04 +0100

    create the calander component

--- pubnak-client/src/components/common/Calendar/Calendar.tsx
+++ pubnak-client/src/components/common/Calendar/Calendar.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
-import Calendar from 'react-calendar';
+import Calendar, { CalendarProps } from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 const CalendarComponent: React.FC = () => {
-  const [value, setValue] = useState(new Date());
+  const [value, setValue] = useState<Date | null>(new Date());
 
-  const handleCalendarChange = (newValue: Date) => {
-    setValue(newValue);
+  const handleCalendarChange: CalendarProps['onChange'] = (newValue) => {
+    if (newValue instanceof Date) {
+      setValue(newValue);
+    }
   };
 
   return (
     <div className="bg-gray-200 rounded-lg shadow-md p-4 mb-5">
       <Calendar
-        // onChange={handleCalendarChange}
+        onChange={handleCalendarChange}
         value={value}
         calendarType="US"
         className="react-calendar"
