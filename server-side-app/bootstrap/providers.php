a9f1825d50fc835cbbeb11c4ba07de6733533919
AnasNAK <anasc00ding@gmail.com>
2024-04-24 15:40:31 +0100

    update the provider

--- server-side-app/bootstrap/providers.php
+++ server-side-app/bootstrap/providers.php
@@ -2,4 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\ServiceServiceProvider::class,
+    App\Providers\RepositoryServiceProvider::class,
+    Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+
 ];
